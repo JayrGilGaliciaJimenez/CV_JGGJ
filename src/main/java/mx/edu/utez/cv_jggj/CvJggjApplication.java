ea9c4ea5b62b09ddc75f08bee68a848620c49232
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-05-27 15:51:21 -0600

    first commit

---
+++ src/main/java/mx/edu/utez/cv_jggj/CvJggjApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cv_jggj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvJggjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvJggjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cv_jggj/CvJggjApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cv_jggj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvJggjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
