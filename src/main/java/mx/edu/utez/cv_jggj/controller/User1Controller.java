ca3c93346daa6571d54f29af4096dcb1a6dd6943
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-05-27 16:13:45 -0600

    [ADD] Se agrego el path de la api

--- src/main/java/mx/edu/utez/cv_jggj/controller/User1Controller.java
+++ src/main/java/mx/edu/utez/cv_jggj/controller/User1Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/user1")
 @CrossOrigin("*")
 public class User1Controller {
     @GetMapping("/")
